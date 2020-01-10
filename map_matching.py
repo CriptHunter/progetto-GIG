cb51488dabf0a3ae4fedbbc7ae61eeaa9bbbf5b3
CriptHunter <lorenzo.dalessandro1@studenti.unimi.it>
2020-01-10 09:48:27 +0100

    new map match fix

--- map_matching.py
+++ map_matching.py
@@ -24,6 +24,8 @@ def fix_isolated_point(plist):
 	for elem in plist[1:-1]:
 		if plist[k-1][1] == plist[k+1][1]:
 			plist[k] = (plist[k][0], plist[k+1][1])
+		elif plist[k-1][1] != elem[1] and elem[1] != plist[k+1][1]:
+			plist[k] = (plist[k][0], plist[k+1][1])
 		k = k + 1
 	return plist
 
