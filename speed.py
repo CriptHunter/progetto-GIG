32da6625e43ce532507b2c15d23349b6ca4bb6de
CriptHunter <lorenzo.dalessandro1@studenti.unimi.it>
2020-01-17 10:21:08 +0100

    fixed speed filterning

--- speed.py
+++ speed.py
@@ -57,7 +57,7 @@ def avg_speed(connection):
 
 	connection.commit()
 
-#inserisce nel database 
+#inserisce nel database le velocità 
 def insert_speed(points, distance_query, insert_query, cursor, speeds_list):
 	for i, p in enumerate(points[:-1]):
 		dt = (points[i+1][1] - points[i][1]).total_seconds()
@@ -68,17 +68,14 @@ def insert_speed(points, distance_query, insert_query, cursor, speeds_list):
 			ds = row[0]
 		speeds_list.append((points[i][0], points[i+1][0], dt, ds))
 
-	speeds_list = list(filter(lambda a: a[3] != 0, speeds_list))
+	speeds_list = list(filter(lambda a: a[3] != 0 and a[2] != 0, speeds_list))
 
 	avg_speed = 0.0
-	zero_points = 0 #numero di punti con Δt = 0
 	for i, e in enumerate(speeds_list):
 		if e[2] != 0:
 			data = (i, e[0], e[1], e[3]/e[2], )
 			avg_speed = avg_speed + e[3]/e[2]
 			cursor.execute(insert_query, data)
-		else:
-			zero_points = zero_points + 1
 
-	avg_speed = avg_speed / (len(speeds_list) - zero_points)
+	avg_speed = avg_speed / len(speeds_list)
 	return avg_speed
